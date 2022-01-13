8a8922d3eb83f561d2305f4e60ce0f26796a8753
steven <stevencr7zz@gmail.com>
2022-01-13 17:39:00 -0600

    Remove null code

--- pages/pirates/[id].tsx
+++ pages/pirates/[id].tsx
@@ -14,8 +14,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async () => {};
-
 export default function Details(): ReactElement {
   return (
     <div>
