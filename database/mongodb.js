0ee7be695f4474c1a76310b61527f9ff23ae9e98
steven <stevencr7zz@gmail.com>
2022-01-14 16:53:07 -0600

    Remove some scripts

--- database/mongodb.js
+++ database/mongodb.js
@@ -12,16 +12,6 @@ let client;
 // separate module, the client can be shared across functions.
 export let clientPromise;
 
-if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// In production mode, it's best to not use a global variable.
+client = new MongoClient(uri, options);
+clientPromise = client.connect();
