de28e1265b3e702995ba16af68c6d33e94571a43
steven <stevencr7zz@gmail.com>
2022-01-14 22:34:55 -0600

    Add console logs

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -2,16 +2,23 @@ import { clientPromise } from "../../database/mongodb";
 
 export default async function handler(req: any, res: any) {
   const client = await clientPromise;
+  console.log("A");
   const db = client.db("nextSkateShopDB");
+  console.log("B");
 
   const category = req.query.category;
+  console.log("C");
 
   if (req.method === "GET") {
+    console.log("D");
     const products = await db.collection("products").find({ category }).toArray();
 
-    if (products == "") {
+    console.log("E");
+    if (products == "F") {
+      console.log("G");
       res.status(400).send("Category was not found in database");
     } else {
+      console.log("H");
       res.json({ status: 200, data: products });
     }
   }
