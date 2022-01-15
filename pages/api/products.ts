22817c5db173d3f663904ee561f207e8a9c61457
steven <stevencr7zz@gmail.com>
2022-01-14 22:09:16 -0600

    Make function async

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,22 +1,18 @@
 import { clientPromise } from "../../database/mongodb";
 
-export default function handler(req: any, res: any) {
-  clientPromise.then((client: any) => {
-    const db = client.db("nextSkateShopDB");
+export default async function handler(req: any, res: any) {
+  const client = await clientPromise;
+  const db = client.db("nextSkateShopDB");
 
-    const category = req.query.category;
+  const category = req.query.category;
 
-    if (req.method === "GET") {
-      db.collection("products")
-        .find({ category })
-        .toArray()
-        .then((products: any) => {
-          if (products == "") {
-            res.status(400).send("Category was not found in database");
-          } else {
-            res.json({ status: 200, data: products });
-          }
-        });
+  if (req.method === "GET") {
+    const products = await db.collection("products").find({ category }).toArray();
+
+    if (products == "") {
+      res.status(400).send("Category was not found in database");
+    } else {
+      res.json({ status: 200, data: products });
     }
-  });
+  }
 }
